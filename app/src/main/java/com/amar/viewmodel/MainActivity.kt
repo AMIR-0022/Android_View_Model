9eb8db8089ef66f60150006cb9ad78ffa52cac00
AMIR-0022 <amir.sohail.1683@gmail.com>
2024-02-05 11:42:47 +0500

    Add View Model Functionality

--- app/src/main/java/com/amar/viewmodel/MainActivity.kt
+++ app/src/main/java/com/amar/viewmodel/MainActivity.kt
@@ -2,10 +2,41 @@ package com.amar.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel;
+    private lateinit var tvCounter: TextView;
+    private lateinit var btnAdd: AppCompatButton;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCounter = findViewById(R.id.tvCounter);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        // ---->> if you don't want to pass parameter to MainViewModel
+        //mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java);
+
+        // ----->> if you want to pass parameter to MainViewModel then you have to create Factory class
+        // that provide the instance of MainViewModel class and can easily pass parameters as well
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(5)).get(MainViewModel::class.java);
+
+        setText();
+
+        btnAdd.setOnClickListener {
+            mainViewModel.increment();
+            setText();
+        }
+
     }
+
+    private fun setText(){
+        tvCounter.text = mainViewModel.count.toString();
+    }
+
 }
--- app/src/main/java/com/amar/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/amar/viewmodel/MainViewModel.kt
@@ -2,6 +2,10 @@ package com.amar.viewmodel
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val initialValue: Int): ViewModel() {
+    var count: Int = initialValue;
 
+    fun increment(){
+        count++;
+    }
 }
